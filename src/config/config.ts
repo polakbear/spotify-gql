d4e80ebd395ffb005688741f80f9bf58612dcd20
sylwia malinowska <sylwia@polakbear.dev>
2022-02-07 19:57:40 +0100

    fix: heroku config fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const config = {
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  port: 4000,
+  port: process.env.PORT || 5000,
 };
 
 export const seeds = {
