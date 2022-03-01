096764cc96005305bfd9d04d74421a51d37be935
sylwia malinowska <sylwia@polakbear.dev>
2022-03-01 20:57:54 +0100

    chore: enabled introspection

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
-import { ApolloServer } from 'apollo-server-express';
-import {typeDefs} from "./schema";
-import {resolvers} from "./resolvers";
-import { createServer } from "http";
-import express from "express";
+import {ApolloServer} from 'apollo-server-express';
+import {typeDefs} from './schema';
+import {resolvers} from './resolvers';
+import {createServer} from 'http';
+import express from 'express';
 // import { prisma } from "./prisma/client";
 
 const startServer = async () => {
   const app = express();
   const httpServer = createServer(app);
 
-  const apolloServer = new ApolloServer({ typeDefs, resolvers })
+  const apolloServer = new ApolloServer({
+    introspection: true,
+    typeDefs,
+    resolvers,
+  });
 
-  await apolloServer.start()
+  await apolloServer.start();
 
-  apolloServer.applyMiddleware({ app, path: `/api` })
+  apolloServer.applyMiddleware({ app, path: `/api` });
 
   httpServer.listen({ port: process.env.PORT || 4000 }, () =>
-    console.log(`Server listening on 4000${apolloServer.graphqlPath}`)
-  )
-}
+    console.log(`Server listening on 4000${apolloServer.graphqlPath}`),
+  );
+};
 
 startServer()
